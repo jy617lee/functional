97cbfe3a23a19c7e92fb6f012bffb850f38c9a7a
jy617lee <jy617.lee@gmail.com>
2023-07-05 21:56:24 +0900

    계산에서 상수를 제외하기

--- 4/june.ts
+++ 4/june.ts
@@ -1,5 +1,6 @@
 // 데이터
 const FREE_SHIPPING_AMOUNT = 20
+const TAX_RATE = 0.1
 interface Item {
   name: string
   price: number
@@ -10,19 +11,16 @@ let shoppingCart: Item[] = []
 let shoppingCartTotal: number = 0
 
 // 계산
-const calcTotalPrice = (shoppingCart: Item[]): number => shoppingCart.reduce(
-  (total, item) => total + item.price, 0)
-const isFreeShipping = (cartTotal: number, newItem: Item): boolean => cartTotal + newItem.price >= 20
-const calculateTax = (shoppingCartTotal: number): number => shoppingCartTotal * 0.1
-
-
+const calcTotalPrice = (cart: Item[]): number => 
+  cart.reduce((total: number, item: Item) => total + item.price, 0)
+const isFreeShipping = (total: number, newItem: Item): boolean => 
+  total + newItem.price >= FREE_SHIPPING_AMOUNT
+const calculateTax = (total: number, taxRate: number): number => total * taxRate
+const addItem = (cart: Item[], item: Item) => [...cart, item]
 
 // 액션
-const addItemToCart = (name: string, price: number) => {
-  shoppingCart.push({
-    name: name,
-    price: price
-  })
+const addItemToCart = (item: Item) => {
+  shoppingCart.push(item)
   calcCartTotal()
 }
 
@@ -38,7 +36,7 @@ const updateShippingIcons = () => {
   }
 }
 
-const updateTaxDom = () => setTaxDom(calculateTax(shoppingCartTotal))
+const updateTaxDom = () => setTaxDom(calculateTax(shoppingCartTotal, TAX_RATE))
 
 const calcCartTotal = () => {
   shoppingCartTotal = calcTotalPrice(shoppingCart)
